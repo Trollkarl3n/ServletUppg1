60ddb51a7d86645dbd58e1a2503578e91cabf6ea
Erik Werther <erik.werther@gmail.com>
2024-02-08 14:18:03 +0100

    Lagt in kod

---
+++ src/main/java/com/TestServletEight/TestServletEight/Servlets/AllCoursesServlet.java
@@ -0,0 +1,56 @@
+package com.TestServletEight.TestServletEight.Servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+
+@WebServlet(urlPatterns = "/course")
+public class AllCoursesServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Skapa en anslutning till databasen
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/root", "root", "");
+
+            // Hämta data från databasen och skapa tabellen
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM courses");
+
+            out.println("<table>");
+            out.println("<tr><th>ID</th><th>Namn</th><th>YHP</th><th>Beskrivning</th></tr>");
+            while (rs.next()) {
+                out.println("<tr>");
+                out.println("<td>" + rs.getInt("id") + "</td>");
+                out.println("<td>" + rs.getString("name") + "</td>");
+                out.println("<td>" + rs.getInt("YHP") + "</td>");
+                out.println("<td>" + rs.getString("description") + "</td>");
+                out.println("</tr>");
+            }
+            out.println("</table>");
+
+            // Lägg till länkar till andra Servlets och startsidan
+            out.println("<a href=\"index.html\">Till startsidan</a>");
+            out.println("<br>");
+            out.println("<a href=\"servlet1\">Visa alla studenter och kurser</a>");
+            out.println("<br>");
+            out.println("<a href=\"servlet2\">Visa alla elever</a>");
+
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+}
